ac90f737cd8cde1bcf34ce4b4ad9a61625ae5f24
anna putilovskaya <putilovskayaam@gmail.com>
2024-12-01 21:26:08 +0300

    create generate_invite_code function

--- users/services.py
+++ users/services.py
@@ -11,3 +11,10 @@ def send_sms(phone_number):
     time.sleep(2)
     print(f"Отправлено SMS на {phone_number} с кодом: {code}")
     return code
+
+
+def generate_invite_code():
+    """
+    Генерирует случайный 6-значный инвайт-код из букв, цифр и спецсимволов.
+    """
+    return "".join(sample(string.digits + string.ascii_letters + string.punctuation, 6))
