0fd02b5880f9a30bb17b49068287e6aac080ed9a
anna putilovskaya <putilovskayaam@gmail.com>
2024-12-01 21:13:03 +0300

    create model user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    """
+    Модель пользователя.
+    """
+
+    username = None
+    password = models.CharField(max_length=128, **NULLABLE, verbose_name="Пароль")
+    phone = PhoneNumberField(unique=True, verbose_name="Телефон")
+    verification_code = models.CharField(
+        max_length=4, **NULLABLE, verbose_name="Код авторизации"
+    )
+    invite_code = models.CharField(
+        max_length=6, unique=True, **NULLABLE, verbose_name="Код приглашения"
+    )
+    invited_by = models.ForeignKey(
+        "self",
+        **NULLABLE,
+        on_delete=models.SET_NULL,
+        verbose_name="Приглашен пользователем",
+        related_name="referrals",
+    )
+
+    USERNAME_FIELD = "phone"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return f"{self.phone}"
